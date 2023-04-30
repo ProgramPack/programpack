9f73d678c66e6bd36372628294c1ef30cb94114a
VB PROGER <97066898+VBPROGER@users.noreply.github.com>
2023-05-01 02:06:09 +0300

    Added interface

--- programpack/__main__.py
+++ programpack/__main__.py
@@ -4,11 +4,16 @@ import programpack as propack
 
 try: argv1 = args[1]
 except IndexError: argv1 = None
+try: argv2 = args[2]
+except IndexError: argv2 = None
 
-if argv1:
-    program = propack.PackedProgram(argv1)
-    program.read()
-    program.run()
-    program.close()
+if argv1 == 'convert':
+    if argv2: propack.convert_file_to_executable(argv2)
+elif argv1 == 'run':
+    if argv2:
+        program = propack.PackedProgram(argv2)
+        program.read()
+        program.run()
+        program.close()
 else:
     print('No args given.')
